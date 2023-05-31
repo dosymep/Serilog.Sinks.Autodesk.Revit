4fff06adcc2926a2d50326c0c3b21f6144cdeda0
dosymep <dosymep@gmail.com>
2023-05-31 09:09:05 +0300

    add use time stamp test

--- src/Serilog.Sinks.Autodesk.Revit.Tests/AutodeskRevitSinkTests.cs
+++ src/Serilog.Sinks.Autodesk.Revit.Tests/AutodeskRevitSinkTests.cs
@@ -1,5 +1,6 @@
 using NUnit.Framework.Internal;
 
+using Serilog.Configuration;
 using Serilog.Core;
 using Serilog.Events;
 using Serilog.Formatting;
@@ -12,9 +13,33 @@ public class AutodeskRevitSinkTests {
         var revitOutput = Helpers.CreateRevitOutput();
         var journalSink = Helpers.CreateJournalSink(revitOutput);
         var logEvent = Helpers.CreateLogEvent("Hello, world!");
-        
+
         journalSink.Emit(logEvent);
-        
+
         StringAssert.Contains("Hello, world!", revitOutput.Comments[0]);
+    }
+
+    [Test]
+    [TestCase(true)]
+    [TestCase(false)]
+    public void UseTimeStampTest(bool useTimeStamps) {
+        var revitOutput = Helpers.CreateRevitOutput();
+        var outputTemplate = Helpers.GetDefaultOutputTemplate();
+        var restrictedToMinimumLevel = LevelAlias.Minimum;
+        LoggingLevelSwitch? levelSwitch = null;
+        IFormatProvider? formatProvider = null;
+
+        var log = new LoggerConfiguration()
+            .WriteTo.RevitJournal(
+                revitOutput,
+                useTimeStamps,
+                outputTemplate,
+                restrictedToMinimumLevel,
+                levelSwitch,
+                formatProvider)
+            .CreateLogger();
+        
+        log.Information("Hello, world!");
+        Assert.That(revitOutput.UseTimeStamp, Is.EqualTo(useTimeStamps));
     }
 }
--- src/Serilog.Sinks.Autodesk.Revit.Tests/RevitOutputTest.cs
+++ src/Serilog.Sinks.Autodesk.Revit.Tests/RevitOutputTest.cs
@@ -1,9 +1,11 @@
 ﻿namespace Serilog.Sinks.Autodesk.Revit.Tests;
 
 public class RevitOutputTest : IRevitOutput {
+    public bool? UseTimeStamp = null;
     public List<string> Comments = new();
-
+    
     public void WriteJournalComment(string comment, bool timeStamp) {
         Comments.Add(comment);
+        UseTimeStamp = timeStamp;
     }
 }
