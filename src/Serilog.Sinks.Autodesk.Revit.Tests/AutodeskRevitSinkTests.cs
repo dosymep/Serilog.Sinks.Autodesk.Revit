7c5be141f2819bc816e1d85abe1f4a4ee03674a9
dosymep <dosymep@gmail.com>
2023-05-31 08:11:37 +0300

    add tests project

---
+++ src/Serilog.Sinks.Autodesk.Revit.Tests/AutodeskRevitSinkTests.cs
@@ -0,0 +1,12 @@
+namespace Serilog.Sinks.Autodesk.Revit.Tests;
+
+public class AutodeskRevitSinkTests {
+    [SetUp]
+    public void Setup() {
+    }
+
+    [Test]
+    public void Test1() {
+        Assert.Pass();
+    }
+}
